b523d37e05722c5785e9d96cb7fb33efc8d644ef
Dominik Pieczyński <dominik.pieczynski@gmail.com>
2020-03-28 15:39:11 +0100

    Restore wheels building and uploading

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ class CMakeBuild(build_ext):
 
     def build_extension(self, ext):
         extdir = os.path.join(os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name))), ext.name)
-        cmake_args = [f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
-                      f'-DPYTHON_EXECUTABLE={sys.executable}']
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
 
         cfg = 'Debug' if self.debug else 'Release'
         build_args = ['--config', cfg]
